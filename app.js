cca125686149d4e7b0c8d9d7eeaad08cd42cbf4c
rupabalan <psriniboise@gmail.com>
2015-06-24 19:26:24 -0600

    commitss

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 $(document).ready( function() {
 	$('.unanswered-getter').submit( function(event){
-		// zero out results if previous search has run
+		// zero out results f previous search has run
 		$('.results').html('');
 		// get the value of the tags the user submitted
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+
+    $('.inspiration-getter').submit( function(event){
+		// zero out results f previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags1 = $(this).find("input[name='answerers']").val();
+		getTopanswers(tags1);
+	});
+
 });
 
 // this function takes the question object returned by StackOverflow 
@@ -88,5 +97,58 @@ var getUnanswered = function(tags) {
 	});
 };
 
+var getTopanswers = function(tags) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	
+	var request = {
+						site: 'stackoverflow',		
+					};
+	
+	var result = $.ajax({
+		url:'http://api.stackexchange.com/2.2/tags/'+tags+'/top-answerers/all_time',
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result) {
+     var html;
+      
+     $.each(result.items, function(index,value){
+        var display_name="";
+        var link_name="";
+        var reputation="";
+        var profile_image="";
+        var user_id="";
+        var user_type="";
+        html="";
+
+        display_name=(value.user.display_name);
+         link_name=(value.user.link)
+         profile_image=(value.user.profile_image);
+         reputation=(value.user.reputation);
+         user_id=(value.user.user_id);
+         user_type=(value.user.user_type);
+
+         html= '<p> User Name:'+display_name+ '<br> Reputation:'+reputation+ '<br>'+
+         '<a href="'+link_name+'">'+
+         '<img src=' + profile_image+ '>'+
+         '</p>';
+
+
+         html= '<dl class="result question">'+
+           '<dt>User Name:'+'</dt>'+
+           '<dd>'+display_name+'</dd>'+
+           '<dt> Reputation:'+'</dt>'+
+           '<dd>'+reputation+ '</dd>'+
+           '<dd><a href="'+link_name+'">'+'</dd>'+
+         '<dd>'+'<img src=' + profile_image+ '>'+ '</dd>'
+         '</dl>';
+$('.results').append(html);
+      });
+ 
+
+});
+};
 
 
